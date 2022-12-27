c66f95c48933136313ae0f5f949fd64cd53e0644
Arpit Taneja <88012@arsd.du.ac.in>
2022-12-27 11:01:48 +0530

    Clean code

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,33 @@
 import createHtmlElement from './create';
+import Icon from './github.png';
 
 function loadFooterContent() {
-  return createHtmlElement('div', ['footer'], null, 'footer');
+  const footer = createHtmlElement('footer', ['footer'], null, null);
+
+  const upperDiv = createHtmlElement('div', null, null, null);
+  const githubLogo = new Image();
+  githubLogo.src = Icon;
+  upperDiv.append(githubLogo);
+  const middleDiv = createHtmlElement('div', null, null, null);
+  const lowerDiv = createHtmlElement(
+    'div',
+    null,
+    null,
+    'The Wednesday Café © 2022 | All Rights Reserved'
+  );
+  const span = createHtmlElement('span', ['heart'], null, '♥');
+  const a = createHtmlElement(
+    'a',
+    null,
+    { href: 'https://github.com/arpittaneja', target: '_blank' },
+    'Arpit Taneja'
+  );
+  middleDiv.append('Made with ', span, ' by ', a);
+  footer.append(upperDiv, middleDiv, lowerDiv);
+  return footer;
 }
 
 export default loadFooterContent;
+
+// {/* <span>Made with <span class="heart">♥</span> by <a href="https://github.com/arpittaneja" target="_blank">Arpit
+//         Taneja</a></span> */}
